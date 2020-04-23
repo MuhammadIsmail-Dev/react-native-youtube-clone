7697d2c063d8f8253119ae3c2e917bf189507867
Ismail <coolkhan138@gmail.com>
2020-04-23 19:45:12 +0500

    First commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/screens/Home';
+
+export default function App() {
+  return (
+    <View>
+      <Home/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
---
+++ src/screens/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Header from '../components/header'
+
+const styles = StyleSheet.create({
+
+});
+
+export default function Home(){
+    return (
+        <View>
+            <Header/>
+        </View>
+    )
+};
